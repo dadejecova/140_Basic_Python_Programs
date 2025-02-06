0499eec09c8d6995e876c84aa3481d619c5c535f
Daniel <114239584+dadejecova@users.noreply.github.com>
2025-02-05 21:57:19 -0500

    Create P_03.py
    Find he area of a triangle

---
+++ P_03.py
@@ -0,0 +1,9 @@
+# Find the area of a triangle
+
+# Input the base and height of the triangle
+base = float(input("Enter the base of the triangle: "))
+height = float(input("Enter the height of the triangle: "))
+# calculate he area
+area = 1/2 * base * height
+# print the area
+print(f"The area of the triangle with base {base} and height {height} is: {area}")
