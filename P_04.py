7838299f661bf06a65f8c5709dc83b6d76a1a5be
Daniel <114239584+dadejecova@users.noreply.github.com>
2025-02-05 21:59:06 -0500

    Create P_04.py
    Find he area of a triangle

---
+++ P_04.py
@@ -0,0 +1,14 @@
+# Swap Two Variables
+
+# input the values of a and b
+a = input("Enter the value of a: ")
+b = input("Enter the value of b: ")
+# print the values before swapping
+print(f"Before swapping a = {a} and b = {b}")
+
+# swapping
+temp = a
+a = b
+b = temp
+# print the values after swapping
+print(f"After swapping a = {a} and b = {b}")
